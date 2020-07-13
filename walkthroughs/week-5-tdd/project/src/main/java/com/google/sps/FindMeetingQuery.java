54152356bfc9208734ff8c9e16a8a8fddb3823d6
Praneet <praneetmek@gmail.com>
2020-07-13 14:03:20 +0000

    completed tdd walkthrough

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,102 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.*;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        Collection<String> attendees = request.getAttendees();
+        Collection<String> optional = request.getOptionalAttendees();
+
+        ArrayList<String> both = new ArrayList<String>();
+
+        for(String attendee: attendees){
+            both.add(attendee);
+        }
+        for(String option: optional){
+            both.add(option);
+        } 
+
+        Collection<TimeRange> optionalSol=queryHelper(both,events,request);
+        if(optionalSol.size()!=0){
+            return optionalSol;
+        }
+        else{
+            return queryHelper(attendees,events,request);
+        }
+
+    }
+
+    public boolean isRelevant(Event event, Collection<String> attendees){
+        for(String attendee : event.getAttendees()){
+            if (attendees.contains(attendee)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Collection<TimeRange> queryHelper(Collection<String> attendees, Collection<Event> events, MeetingRequest request){
+        ArrayList<Integer> importantTimes = new ArrayList<Integer>();
+        HashMap<Integer,Integer> eventCheckVals= new HashMap<Integer,Integer>();
+        System.out.println("STARTING________");
+        for(Event event : events){
+            System.out.println("Time Range:" + event.getWhen().start() + "-" + event.getWhen().end());
+            if (isRelevant(event,attendees)){
+                int start = event.getWhen().start();
+                int end = event.getWhen().end();
+                importantTimes.add(start);
+                importantTimes.add(end);
+                if (eventCheckVals.containsKey(start)){
+                    eventCheckVals.put(start,eventCheckVals.get(start)+1);
+                }
+                else{
+                    eventCheckVals.put(start,1);
+                }
+
+                if (eventCheckVals.containsKey(end)){
+                    eventCheckVals.put(end,eventCheckVals.get(end)-1);
+                }
+                else{
+                    eventCheckVals.put(end,-1);
+                }
+            }
+ 
+        }
+        //importantTimes.add(TimeRange.END_OF_DAY);
+        Collections.sort(importantTimes);
+
+        Collection<TimeRange> sol = new ArrayList<>();
+        int counter=0;
+        int last_time=0;
+
+        if(request.getDuration()>=1440){
+            return sol;
+        }
+
+        if (importantTimes.size()==0){
+            sol.add(TimeRange.WHOLE_DAY);
+            return sol;
+        }
+
+        for(int time: importantTimes){
+
+            if (counter==0 && counter + eventCheckVals.get(time)>=0 && time-last_time >= request.getDuration()){
+                TimeRange timeRangeToAdd = TimeRange.fromStartDuration(last_time, time-last_time);
+                sol.add(timeRangeToAdd);
+            }
+            last_time=time;
+            counter += eventCheckVals.get(time);
+
+        }
+
+        if (TimeRange.END_OF_DAY-last_time >= request.getDuration()){
+            TimeRange timeRangeToAdd = TimeRange.fromStartEnd(last_time,TimeRange.END_OF_DAY,true);
+            sol.add(timeRangeToAdd);
+        }
+
+        return sol;
+
+
+    }
 }
